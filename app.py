7597482f9325d5fb45953adca3009bd65a9d41ec
MKim <mrn.kim07@gmail.com>
2023-04-20 11:49:31 -0700

    add root route

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get('/')
+def show_survey_start():
+    #title, instructions
+    title = survey.title
+    instructions = survey.instructions
+
+    return render_template("survey_start.html", title=title, instructions=instructions)
